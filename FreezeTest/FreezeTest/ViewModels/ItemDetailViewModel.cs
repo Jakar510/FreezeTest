f7814bc4f8265bfbb4639e192df4ceefdb13718a
Tyler Stegmaier <jakar510@gmail.com>
2020-06-17 09:23:23 -0500

    Initial Commit

---
+++ FreezeTest/FreezeTest/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using FreezeTest.Models;
+
+namespace FreezeTest.ViewModels
+{
+	public class ItemDetailViewModel : BaseViewModel
+	{
+		public Item Item { get; set; }
+		public ItemDetailViewModel(Item item = null)
+		{
+			Title = item?.Text;
+			Item = item;
+		}
+	}
+}
